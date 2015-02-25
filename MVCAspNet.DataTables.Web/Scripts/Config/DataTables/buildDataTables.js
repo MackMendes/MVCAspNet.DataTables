ee75bc917ea5c0c7ba32f02745f09d4489a564db
Charles Mendes de Macedo <charlesmendes_31@hotmail.com>
2015-02-25 00:04:07 -0300

    Comentando exemplos de utilização.

--- MVCAspNet.DataTables.Web/Scripts/Config/DataTables/buildDataTables.js
+++ MVCAspNet.DataTables.Web/Scripts/Config/DataTables/buildDataTables.js
@@ -1,30 +1,53 @@
 ﻿// ********* Arquivo .js para configuração e atribuição do Plugin DataTables. *********
 // Criado por: Charles Mendes de Macedo
 /** Como utilizar:
-     Basicamente para utilizar é necessário que a Table(<table>) tenha a classe "_tbDataTables". Essa table têm que ter a separação de <thead> <tbody> no mínimo!
+     Basicamente para utilizar é necessário que a Table(<table>) tenha a classe "_tbDataTables". Essa table tem que ter a separação de <thead> <tbody>!
      ** Forma de atribuir o Plugin:
       * Básica: 
-          Não atribuindo nenhum atribudo para sua table, o função para carregar (PageDataTables.Load()) vai entender que é para atribuir o plugin depois
-          que a table já foi preechida.
-              Ex.: <table class="_tbDataTables"><thead><tr><th>RA</th><th>Nome do Aluno</th></tr></thead><tbody></tbody></table>
+          Não adicionando nenhum dos atribudos para sua table, o função (PageDataTables.Load()) vai atribuir o plugin depois que a table já foi preechida.
+            Ex.: 
+            <table class="_tbDataTables">
+                <thead>
+                    <tr>
+                        <th>IdCliente</th>
+                        <th>Cliente</th>
+                    </tr>
+                </thead>
+                <tbody>
+                </tbody>
+            </table>
       * Carregamento Ajax somente no Inicio:
-          Atribuindo na sua tabela (<table>) o ajaxsource (WebService do Ajax) com o caminho do WebService o destroy como "true" o processing como "true".
-          No seu <thead> nos <th> o column com o nome do atributo que vai vindo no json do WebService consumido.
+          Adicionando na sua tabela (<table>) o atributo "ajaxsource" (WebService do Ajax) com o caminho do WebService, 
+          o atributo "destroy" tem que ser "true" e colocar o atribulo processing (opcional).
+          Na tag <thead> nos <th> o atributo "column" precisa ser preenchido com o nome dos atributos 
+          do json que será resultado do WebService consumido.
               Ex.: <table class="_tbDataTables" ajaxsource="Aluno/ProcessarAoIniciar" destroy="true" processing="true">
-                     <thead><tr><th column="nr_ra_aluno">RA</th><th column="nm_aluno">Nome do Aluno</th></tr></thead>
+                     <thead>
+                        <tr>
+                            <th column="idCliente">ID Cliente</th>
+                            <th column="cliente">Nome do Cliente</th>
+                        </tr>
+                     </thead>
                      <tbody></tbody>
                    </table>
       * Processamento 100% Ajax:
-          Atribundo na sua tabela (<table>) o ajaxsource (WebService do Ajax) com o caminho do WebService e o atribudo serverside como true.  No seu <thead> 
-          nos <th> o column com o nome do atributo que vai vindo no json do WebService consumido.
-              Ex.: <table class="_tbDataTables" ajaxsource="Aluno/ProcessarDataTables">
-                     <thead><tr><th column="nr_ra_aluno">RA</th><th column="nm_aluno">Nome do Aluno</th></tr></thead>
-                     <tbody></tbody>
-                   </table>
+          Adicionando na tabela (<table>) o atributo "ajaxsource" (WebService do Ajax) com o caminho do WebService, o atributo "serverside" tem que ser 
+          colocodo como "true". Na tag <thead> nos <th> o atributo "column" precisa ser preenchido com o nome dos atributos do json que será resultado do 
+          WebService consumido.
+              Ex.: 
+                <table class="_tbDataTables" ajaxsource="Aluno/ProcessarDataTables">
+                    <thead>
+                    <tr>
+                        <th column="IdCliente">ID Cliente</th>
+                        <th column="Cliente">Nome do Cliente</th>
+                    </tr>
+                    </thead>
+                    <tbody></tbody>
+                </table>
      ** Atributos:
-       Como descrito acima, para utilizar esse .js é necessário colocar atributos na tabela (<table>), tentei pegar os principais na função de 
-       carregar (PageDataTables.Load()), mas nada impede de implementar mais no decorrer de novas necessidade!
-       Os atributos que ela procura na tabela <table>:
+       Como descrito acima, para utilizar esse .js é necessário colocar atributos na tabela (<table>), tentei pegar as principais features do DataTables 
+       Jquery na função de carregar (PageDataTables.Load()), mas nada impede de implementar mais no decorrer de novas necessidade!
+       Os atributos que a função deste JS vai procura na tabela <table>:
           - ajaxSource: WebService do Ajax
           - serverSide: Processamento no servidor? (boolean)
           - jqueryUI: Utiliza o estilo (style) do JqueryUI? (boolean)
@@ -34,7 +57,7 @@
           - destroy: Se vai ser Destruído TUDO ao carregar (boolean)
           - processing: Se irá mostrar o Show Processando (boolean)
     
-       Os atributos que ela procura no Head (<thead><tr><th>) da tabela (<table>):
+       Os atributos que a função deste JS vai procura no Head (<thead><tr><th>) da tabela (<table>):
           - column: Nome do atributo que vai vindo no json do WebService consumido.
           - sortable: Se a coluna que possui esse atributo vai ser ordenada ou não (boolean)
           - visible: Se a coluna que possui esse atributo vai ser visível ou não (boolean)
