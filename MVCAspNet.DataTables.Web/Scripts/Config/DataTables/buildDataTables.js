bb8edae80ab64e841959ebc8f6e24a9620cc515a
Charles Mendes de Macedo <MackMendes@users.noreply.github.com>
2015-12-16 18:11:05 -0200

    Refotorando
    Refotorando

--- MVCAspNet.DataTables.Web/Scripts/Config/DataTables/buildDataTables.js
+++ MVCAspNet.DataTables.Web/Scripts/Config/DataTables/buildDataTables.js
@@ -1,10 +1,10 @@
-﻿// ********* Arquivo .js para configuração e atribuição do Plugin DataTables. *********
+// ********* Arquivo .js para configuração e atribuição do Plugin DataTables. *********
 // Criado por: Charles Mendes de Macedo
 /** Como utilizar:
      Basicamente para utilizar é necessário que a Table(<table>) tenha a classe "_tbDataTables". Essa table tem que ter a separação de <thead> <tbody>!
      ** Forma de atribuir o Plugin:
       * Básica: 
-          Não adicionando nenhum dos atribudos para sua table, o função (PageDataTables.Load()) vai atribuir o plugin depois que a table já foi preechida.
+          Não adicionando nenhum dos atribudos para sua table, o função (BuildDataTables.Load()) vai atribuir o plugin depois que a table já foi preechida.
             Ex.: 
             <table class="_tbDataTables">
                 <thead>
@@ -46,7 +46,7 @@
                 </table>
      ** Atributos:
        Como descrito acima, para utilizar esse .js é necessário colocar atributos na tabela (<table>), tentei pegar as principais features do DataTables 
-       Jquery na função de carregar (PageDataTables.Load()), mas nada impede de implementar mais no decorrer de novas necessidade!
+       Jquery na função de carregar (BuildDataTables.Load()), mas nada impede de implementar mais no decorrer de novas necessidade!
        Os atributos que a função deste JS vai procura na tabela <table>:
           - ajaxSource: WebService do Ajax
           - serverSide: Processamento no servidor? (boolean)
@@ -63,153 +63,158 @@
           - visible: Se a coluna que possui esse atributo vai ser visível ou não (boolean)
 */
 
-// ************************************************* //
-// ** Objecto para armazenar as functions criadas ** //
-var PageDataTables = {};
-
 // Carregar o DataTable após carregar tudo
 // Author: Charles Mendes de Macedo
-$(document).ready(function () { PageDataTables.Load() });
+$(document).ready(function () { BuildDataTables.Load() });
 
-// Função que Carrega o DataTables em todos as table's que tiver com o classe _tbDataTables
-// Author: Charles Mendes de Macedo
-PageDataTables.Load = function () {
-    var tables = $('._tbDataTables');
-    for (var i = 0; i < tables.length; i++) {
-        var table = $(tables[i]);
-        table.dataTable(PageDataTables.GetAtributos(table));
-    }
-}
+// ************************************************* //
+// ** Objecto para armazenar as functions criadas ** //
+var BuildDataTables = {
 
-// Pegar todos os atributos da table para montar Json que o plugin (DataTables) utiliza
-// Author: Charles Mendes de Macedo
-PageDataTables.GetAtributos = function (objTable) {
-    var atributos = {};
-    //**********************************************************************************///
-    /* Iniciando os atributos (Default) */
-    atributos.aoColumns = [];                                           // Colunas
-    atributos.sAjaxSource = null;                                       // WebService do Ajax
-    atributos.bServerSide = false;                                      // Processamento no servidor? (boolean)
-    atributos.sPaginationType = "full_numbers";                         // Tipo de Paginação
-    atributos.aLengthRows = 10;                                         // Quantidade de Linhas ao carregar
-    atributos.aLengthMenu = [[10, 25, 50, -1], [10, 25, 50, "Todos"]];  // Menu para mostrar quantidade de registros por página para o usuário
-    atributos.oLanguage = getInternationalisation();                    // Tradução para pt-BR
-    atributos.jQueryUI = false;                                         // Utiliza o estilo (style) do JqueryUI? (boolean)
-    atributos.bFilter = true;                                           // Se vai ter o filtro do cabeçalho ou não (boolean)
-    atributos.bLengthChange = true;                                     // Se vai ter o menu que contém quantidade de linhas por página  (boolean)
-    atributos.sServerMethod = enumMethodServer.get;                     // Método do Servidor (Web Servicer) será GET ou POST
-    atributos.bDestroy = false;                                         // Se vai ser Destruído TUDO ao carregar (boolean)
-    atributos.bProcessing = false;                                      // Se irá mostrar o Show Processando (boolean)
-
-    //**********************************************************************************//
-    /* Pegando os atributos no Objeto Table (jquery) passado pelo parametro de entrada */
-
-    // Pegando o nome das atributos do Json para cada coluna na ordem colocado no thead ///
-    var tableTh = objTable.children('thead').children('tr').children('th');
-    if (tableTh !== undefined) {
-        for (var i = 0; i < tableTh.length; i++) {
-            var attrColumn = {};
-            var jquerTH = $(tableTh[i]);
-
-            attrColumn.bSortable = (jquerTH.attr('sortable') !== 'false');
-            attrColumn.bVisible = (jquerTH.attr('visible') !== 'false');
-            var dtColumn = jquerTH.attr('column');
-            if (dtColumn !== undefined) {
-                attrColumn.mData = dtColumn.toString();
-            }
-
-            atributos.aoColumns.push(attrColumn);
+    // Função que Carrega o DataTables em todos as table's que tiver com o classe _tbDataTables
+    // Author: Charles Mendes de Macedo
+    Load: function () {
+        var tables = $('._tbDataTables');
+        for (var i = 0; i < tables.length; i++) {
+            var $table = $(tables[i]);
+            $table.dataTable(BuildDataTables.GetAtributos($table));
         }
-    }
+    },
 
-    // Pegando o caminho do Ajax 
-    var ajaxSource = objTable.attr('ajaxSource');
-    if (ajaxSource !== undefined)
-        atributos.sAjaxSource = ajaxSource.toString();
+    // Pegar todos os atributos da table para montar Json que o plugin (DataTables) utiliza
+    // Author: Charles Mendes de Macedo
+    GetAtributos: function ($objTable) {
+        var atributos = {};
+        //**********************************************************************************///
+        /* Iniciando os atributos (Default) */
+        atributos.aoColumns = [];                                           // Colunas
+        atributos.sAjaxSource = null;                                       // WebService do Ajax
+        atributos.bServerSide = false;                                      // Processamento no servidor? (boolean)
+        atributos.sPaginationType = "full_numbers";                         // Tipo de Paginação
+        atributos.aLengthRows = 10;                                         // Quantidade de Linhas ao carregar
+        atributos.aLengthMenu = [[10, 25, 50, -1], [10, 25, 50, "Todos"]];  // Menu para mostrar quantidade de registros por página para o usuário
+        atributos.oLanguage = BuildDataTables.GetInternationalisation();    // Tradução para pt-BR
+        atributos.jQueryUI = false;                                         // Utiliza o estilo (style) do JqueryUI? (boolean)
+        atributos.bFilter = true;                                           // Se vai ter o filtro do cabeçalho ou não (boolean)
+        atributos.bLengthChange = true;                                     // Se vai ter o menu que contém quantidade de linhas por página  (boolean)
+        atributos.sServerMethod = BuildDataTables.enumMethodServer.get;     // Método do Servidor (Web Servicer) será GET ou POST
+        atributos.bDestroy = false;                                         // Se vai ser Destruído TUDO ao carregar (boolean)
+        atributos.bProcessing = false;                                      // Se irá mostrar o Show Processando (boolean)
 
-    // Pegando se o processamento será no servidor
-    atributos.bServerSide = objTable.attr('serverSide') === 'true';
+        //*********************************************************************************///
+        /* Pegando os atributos no Objeto Table (jquery) passado pelo parametro de entrada */
 
-    // Pegando se será utilizado o estilo do JqueryUI
-    atributos.jQueryUI = objTable.attr('jqueryUI') === 'true';
+        // Pegando o nome das atributos do Json para cada coluna na ordem colocado no thead ///
+        var tableTh = $objTable.find('thead tr th');
 
-    // Pegando se vai ter o filtro do cabeçalho
-    atributos.bFilter = objTable.attr('filter') !== 'false';
+        if (tableTh) {
+            
+            var jquerTH, dtColumn;
+            var tableThLength = tableTh.length;
+            for (var i = 0; i < tableThLength; i++) {
+                var attrColumn = {};
+                jquerTH = $(tableTh[i]);
 
-    // Pegando se vai poder alterar a quantidade de linhas por página
-    atributos.bLengthChange = objTable.attr('lengthChange') !== 'false';
+                attrColumn.bSortable = (jquerTH.attr('sortable') !== 'false');
+                attrColumn.bVisible = (jquerTH.attr('visible') !== 'false');
+                dtColumn = jquerTH.attr('column');
+                if (dtColumn) {
+                    attrColumn.mData = dtColumn.toString();
+                }
 
-    // Tipo do Método no Servidor (POST ou GET)
-    var serverMethod = objTable.attr('serverMethod');
-    if (serverMethod !== undefined)
-        atributos.sServerMethod = (serverMethod.toString() === enumMethodServer.post ? enumMethodServer.post : enumMethodServer.get);
+                atributos.aoColumns.push(attrColumn);
+            }
+        }
 
-    // Se vai ser Destruído TUDO ao carregar
-    atributos.bDestroy = objTable.attr('destroy') === 'true';
+        // Pegando o caminho do Ajax 
+        var ajaxSource = $objTable.attr('ajaxSource');
+        if (ajaxSource)
+            atributos.sAjaxSource = ajaxSource.toString();
 
-    // Se vai ter o processamento ao carregar
-    var processing = objTable.attr('processing');
-    if (processing !== undefined)
-        atributos.bProcessing = (processing === 'true');
+        // Pegando se o processamento será no servidor
+        atributos.bServerSide = $objTable.attr('serverSide') === 'true';
 
+        // Pegando se será utilizado o estilo do JqueryUI
+        atributos.jQueryUI = $objTable.attr('jqueryUI') === 'true';
 
-    return atributos;
-}
+        // Pegando se vai ter o filtro do cabeçalho
+        atributos.bFilter = $objTable.attr('filter') !== 'false';
 
-// Pegar a internalização do DataTables em Português (pt-BR)
-// Author: Charles Mendes de Macedo
-function getInternationalisation() {
-    // Tradução do DataTables
-    if (PageDataTables.ConfigLanguage === undefined) {
-        PageDataTables.ConfigLanguage = {
-            "oAria": {
-                "sSortAscending": ": ativar para classificar coluna ascendente",
-                "sSortDescending": ": ativar para classificar coluna descendente"
-            },
-            "oPaginate": {
-                "sFirst": "Primeiro",
-                "sLast": "&Uacute;ltimo",
-                "sNext": "Pr&oacute;ximo",
-                "sPrevious": "Anterior"
-            },
-            "sEmptyTable": "N&atilde;o h&aacute; dados dispon&iacute;veis na tabela",
-            "sInfo": "Exibindo de _START_ a _END_ de _TOTAL_ registros",
-            "sInfoEmpty": "Exibindo 0 a 0 de 0 registros",
-            "sInfoFiltered": "(filtrado a partir de _MAX_ registros)",
-            "sInfoPostFix": "",
-            "sInfoThousands": ",",
-            "sLengthMenu": "Mostrar _MENU_",
-            "sLoadingRecords": "Carregando...",
-            "sProcessing": "Processando...",
-            "sSearch": "Pesquisar:",
-            "sZeroRecords": "Nenhum registro encontrado com o filtro utilizado",
-            /**
-             * All of the language information can be stored in a file on the
-             * server-side, which DataTables will look up if this parameter is passed.
-             * It must store the URL of the language file, which is in a JSON format,
-             * and the object has the same properties as the oLanguage object in the
-             * initialiser object (i.e. the above parameters). Please refer to one of
-             * the example language files to see how this works in action.
-             *  @type string
-             *  @default <i>Empty string - i.e. disabled</i>
-             *  @dtopt Language
-             * 
-             *  @example
-             *    $(document).ready( function() {
-             *      $('#example').dataTable( {
-             *        "oLanguage": {
-             *          "sUrl": "http://www.sprymedia.co.uk/dataTables/lang.txt"
-             *        }
-             *      } );
-             *    } );
-             */
-            "sUrl": ""
-        };
-    }
+        // Pegando se vai poder alterar a quantidade de linhas por página
+        atributos.bLengthChange = $objTable.attr('lengthChange') !== 'false';
 
-    return PageDataTables.ConfigLanguage;
-}
+        // Tipo do Método no Servidor (POST ou GET)
+        var serverMethod = $objTable.attr('serverMethod');
+        if (serverMethod)
+            atributos.sServerMethod = (serverMethod === BuildDataTables.enumMethodServer.post ? BuildDataTables.enumMethodServer.post :
+                BuildDataTables.enumMethodServer.get);
 
-// Enum do tipo de Método no sevidor
-// Author: Charles Mendes de Macedo
-var enumMethodServer = { get: "GET", post: "POST" };
+        // Se vai ser Destruído TUDO ao carregar
+        atributos.bDestroy = $objTable.attr('destroy') === 'true';
+
+        // Se vai ter o processamento ao carregar
+        var processing = $objTable.attr('processing');
+        if (processing)
+            atributos.bProcessing = (processing === 'true');
+
+        return atributos;
+    },
+
+    // Pegar a internalização do DataTables em Português (pt-BR)
+    // Author: Charles Mendes de Macedo
+    GetInternationalisation: function () {
+        // Tradução do DataTables
+        if (BuildDataTables.ConfigLanguage === undefined) {
+            BuildDataTables.ConfigLanguage = {
+                "oAria": {
+                    "sSortAscending": ": ativar para classificar coluna ascendente",
+                    "sSortDescending": ": ativar para classificar coluna descendente"
+                },
+                "oPaginate": {
+                    "sFirst": "Primeiro",
+                    "sLast": "&Uacute;ltimo",
+                    "sNext": "Pr&oacute;ximo",
+                    "sPrevious": "Anterior"
+                },
+                "sEmptyTable": "N&atilde;o h&aacute; dados dispon&iacute;veis na tabela",
+                "sInfo": "Exibindo de _START_ a _END_ de _TOTAL_ registros",
+                "sInfoEmpty": "Exibindo 0 a 0 de 0 registros",
+                "sInfoFiltered": "(filtrado a partir de _MAX_ registros)",
+                "sInfoPostFix": "",
+                "sInfoThousands": ",",
+                "sLengthMenu": "Mostrar _MENU_",
+                "sLoadingRecords": "Carregando...",
+                "sProcessing": "Processando...",
+                "sSearch": "Pesquisar:",
+                "sZeroRecords": "Nenhum registro encontrado com o filtro utilizado",
+                /**
+                 * All of the language information can be stored in a file on the
+                 * server-side, which DataTables will look up if this parameter is passed.
+                 * It must store the URL of the language file, which is in a JSON format,
+                 * and the object has the same properties as the oLanguage object in the
+                 * initialiser object (i.e. the above parameters). Please refer to one of
+                 * the example language files to see how this works in action.
+                 *  @type string
+                 *  @default <i>Empty string - i.e. disabled</i>
+                 *  @dtopt Language
+                 * 
+                 *  @example
+                 *    $(document).ready( function() {
+                 *      $('#example').dataTable( {
+                 *        "oLanguage": {
+                 *          "sUrl": "http://www.sprymedia.co.uk/dataTables/lang.txt"
+                 *        }
+                 *      } );
+                 *    } );
+                 */
+                "sUrl": ""
+            };
+        }
+
+        return BuildDataTables.ConfigLanguage;
+    },
+
+    // Enum do tipo de Método no sevidor
+    // Author: Charles Mendes de Macedo
+    enumMethodServer: { get: "GET", post: "POST" }
+};
